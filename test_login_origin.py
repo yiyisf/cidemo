11750a19b16dbde33b9f461387c1a0740bfaaced
liuzhe <779402401@qq.com>
2019-12-22 13:39:17 +0800

    add options

--- test_login_origin.py
+++ test_login_origin.py
@@ -14,16 +14,22 @@ logging.basicConfig(filename="tsg.log", level=logging.INFO, format='%(asctime)s 
 class TestTsg:
     def setup(self):
 
-        opt = Options()
-        # opt.set_headless()
-        opt.add_argument('--no-sandbox')
-        # 让Chrome在root权限下跑
-        opt.add_argument('--disable-dev-shm-usage')
-        opt.add_argument('--headless')
-        opt.add_argument('blink-settings=imagesEnabled=false')
-        opt.add_argument('--disable-gpu')
+        # opt = Options()
+        # # opt.set_headless()
+        # opt.add_argument('--no-sandbox')
+        # # 让Chrome在root权限下跑
+        # opt.add_argument('--disable-dev-shm-usage')
+        # opt.add_argument('--headless')
+        # opt.add_argument('blink-settings=imagesEnabled=false')
+        # opt.add_argument('--disable-gpu')
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--headless')
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
 
-        self.driver = webdriver.Chrome()
+
+        # self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(10)
         self.driver.get("https://fxm5547.baobaobooks.com/")
         assert "小明fxm5547" in self.driver.title
