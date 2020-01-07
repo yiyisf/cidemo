4c640a918d063ebab7c024f159e6e6a1f48f2d6e
liuzhe <779402401@qq.com>
2020-01-08 00:11:52 +0800

    upd

--- testcase/test_login_origin.py
+++ testcase/test_login_origin.py
@@ -23,8 +23,9 @@ class TestTsg:
         # self.driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', chrome_options=chromeOptions)
         self.driver = webdriver.Chrome(options=chromeOptions)
         self.driver.implicitly_wait(10)
-        self.driver.get("https://fxm5547.baobaobooks.com/")
-        assert "小明fxm5547" in self.driver.title
+        # self.driver.get("https://fxm5547.baobaobooks.com/")
+        # assert "小明fxm5547" in self.driver.title
+        self.driver.get("https://www.jianshu.com/p/c16498b6b1c1")
         sleep(5)
         self.driver.get_screenshot_as_file('firstpage.png')
 
